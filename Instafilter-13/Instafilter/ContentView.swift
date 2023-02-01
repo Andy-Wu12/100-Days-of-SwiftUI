9f4e7c8adadfaab86e62a53500e9d31e46b881e6
Andy Wu <awpats12@gmail.com>
2023-01-31 23:54:24 -0500

    Project 13 initial commit

---
+++ Instafilter-13/Instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Andy Wu on 1/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("InstaFilter")
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter-13/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Andy Wu on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
