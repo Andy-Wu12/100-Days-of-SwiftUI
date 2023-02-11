53b4775ac28be79d2cdd3ee04e836bac5ae61d1d
Andy Wu <awpats12@gmail.com>
2023-02-11 15:02:51 -0500

    Simplified coordinate code in ContentView

--- BucketList-14/MapLocation.swift
+++ BucketList-14/MapLocation.swift
@@ -5,7 +5,7 @@
 //  Created by Andy Wu on 2/11/23.
 //
 
-import Foundation
+import MapKit
 
 struct MapLocation: Identifiable, Codable, Equatable {
     let id: UUID
@@ -14,4 +14,18 @@ struct MapLocation: Identifiable, Codable, Equatable {
     var latitude: Double
     var longitude: Double
     
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    // Examples are great to use in previews
+    static let example = MapLocation(id: UUID(), name: "Buckingham Palace",
+                                     description: "Where Quuen Elizabeth lived",
+                                     latitude: 51.501, longitude: -0.141)
+    
+    // Override SwiftUI's provided == from Equatable
+        // which (wastefully in our case) compares every property by default
+    static func ==(lhs: MapLocation, rhs: MapLocation) -> Bool {
+        lhs.id == rhs.id
+    }
 }
