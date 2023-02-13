20c8b06bc99fb1b819278cd728a7d361fa1045a2
Andy Wu <awpats12@gmail.com>
2023-02-12 22:31:58 -0500

    Fetch nearby places for map annotation

---
+++ BucketList-14/BucketList-14/WikipediaResult.swift
@@ -0,0 +1,23 @@
+//
+//  WikipediaResult.swift
+//  BucketList-14
+//
+//  Created by Andy Wu on 2/12/23.
+//
+
+import Foundation
+
+// This  data is structed according to the Wikipedia API's JSON data format
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int : Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
