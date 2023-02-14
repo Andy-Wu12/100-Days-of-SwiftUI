073b079130c139be6d7aecbd3813f4117dde5486
Andy Wu <awpats12@gmail.com>
2023-02-14 17:32:24 -0500

    Challenge 3 - Added ViewModel for Edit View

--- BucketList-14/BucketList-14/Views/EditView.swift
+++ BucketList-14/BucketList-14/Views/EditView.swift
@@ -12,37 +12,31 @@ struct EditView: View {
         case loading, loaded, failed
     }
     
+    @StateObject private var viewModel: ViewModel
+    
     @Environment(\.dismiss) var dismiss
+    
     var location: MapLocation
-    
-    // Init props
-    @State private var name: String
-    @State private var description: String
-    
     var onSave: (MapLocation) -> Void
-    
-    // Network request props
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
     
     init(location: MapLocation, onSave: @escaping (MapLocation) -> Void) {
         self.location = location
         self.onSave = onSave
         
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
+        _viewModel = StateObject(wrappedValue: ViewModel(location: location))
     }
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Place name", text: $name)
-                    TextField("Description", text: $description)
+                    TextField("Place name", text: $viewModel.name)
+                    TextField("Description", text: $viewModel.description)
                 }
                 Section("Nearby...") {
-                    switch loadingState {
+                    switch viewModel.loadingState {
                     case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
+                        ForEach(viewModel.pages, id: \.pageid) { page in
                             // Notice how we use + here to add tet views together
                                 // that mix and match different kinds of formatting
                             Text(page.title)
@@ -63,41 +57,19 @@ struct EditView: View {
                 Button("Save") {
                     var newLocation = location
                     newLocation.id = UUID()
-                    newLocation.name = name
-                    newLocation.description = description
+                    newLocation.name = viewModel.name
+                    newLocation.description = viewModel.description
                     
                     onSave(newLocation)
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces()
             }
         }
     }
-    
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            // Data received
-            let items = try JSONDecoder().decode(Result.self, from: data)
-            
-            // success - convert array values to our pages array
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            // if we're still here it means the request failed somehow
-            loadingState = .failed
-        }
-    }
+
 }
 
 struct EditView_Previews: PreviewProvider {
