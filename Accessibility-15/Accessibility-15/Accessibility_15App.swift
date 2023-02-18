b1a16feb876ad091305238b0c2e1ca98e7add38a
Andy Wu <awpats12@gmail.com>
2023-02-17 20:26:40 -0500

    Project 15 - Initial commit

---
+++ Accessibility-15/Accessibility-15/Accessibility_15App.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_15App.swift
+//  Accessibility-15
+//
+//  Created by Andy Wu on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
