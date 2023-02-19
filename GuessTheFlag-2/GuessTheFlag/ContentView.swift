d3012390e156989d55c5a389f6a8519863e33c98
Andy Wu <awpats12@gmail.com>
2023-02-19 18:12:23 -0500

    Added accessibility for GuessTheFlag

--- GuessTheFlag-2/GuessTheFlag/ContentView.swift
+++ GuessTheFlag-2/GuessTheFlag/ContentView.swift
@@ -19,6 +19,29 @@ struct ContentView: View {
 
     @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US", "Argentina", "Brazil", "Canada", "Columbia", "Greece", "Japan", "Monaco", "South Korea", "Switzerland", "Ukraine"].shuffled()
     @State var correctAnswer = Int.random(in: 0...2)
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner",
+        "Argentina": "Flag with three equally wide horizontal bands alternating sky blue and white in color. The white band in the middle has a yellow Sun of May",
+        "Brazil": "Flag with green background and a yellow rhombus with blue disc depicting a starry sky in the center",
+        "Canada": "Flag with a red background and white square in the center with a red maple leaf",
+        "Columbia": "Flag with horizontal stripes of yellow, blue, and red from top to bottom. The yellow stripe is 2 times larger than the rest.",
+        "Greece": "Flag with nine equal horizontal stripes of blue alternating with white. There is a blue square in the upper left corner bearing a white cross.",
+        "Japan": "Flag is completely white except for red circle in the center",
+        "Monaco": "Flag with two equal horizontal bands of red and white from top to bottom",
+        "South Korea": "Flag with a white rectangular background, a red and blue taegeuk in it's center, along with a black trigram on each of the four corners.",
+        "Switzerland": "Flag with a white cross in the center of a square red field",
+        "Ukraine": "Flag with two equal horizontal bands of blue and yellow, from top to bottom."
+    ]
     
     @State private var roundsPlayed = 0
     @State private var gameOver = false
@@ -59,6 +82,7 @@ struct ContentView: View {
                         .opacity(opacityLevel[number])
                         .shadow(color: guessed ? (number == correctAnswer ? .green : .red) : .clear,
                                 radius: 5)
+                        .accessibilityLabel(labels[countries[number], default: "Unknown country"])
                     }
                 } .frame(maxWidth: .infinity)
                     .padding(.vertical, 20)
