9add575220447ae96dc4fbd8ec6b4de622cd8abb
Andy Wu <awpats12@gmail.com>
2023-02-20 23:31:29 -0500

    Project 16 - inital commit

---
+++ HotProspects-16/HotProspects-16/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HotProspects-16
+//
+//  Created by Andy Wu on 2/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hot Prospects - Project 16")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects-16/HotProspects-16/HotProspects_16App.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspects_16App.swift
+//  HotProspects-16
+//
+//  Created by Andy Wu on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspects_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
